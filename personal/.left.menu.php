81c2b89de8bbe3f797c07e30f08cd5c66c5644c2
Ivan Ivanov <nuclearfeces@gmail.com>
2018-05-02 13:36:23 +0400

    git-repository creation

---
+++ personal/.left.menu.php
@@ -0,0 +1,39 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Настройки пользователя", 
+		"/personal/profile/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Персональный рабочий стол", 
+		"/personal/desktop.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Универсальные списки", 
+		"/personal/lists/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Подписка", 
+		"/personal/subscribe/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Счет пользователя", 
+		"/personal/account/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
---
+++ urlrewrite.php
@@ -0,0 +1,139 @@
+<?php
+$arUrlRewrite=array (
+  15 => 
+  array (
+    'CONDITION' => '#^/examples/my-components/news/#',
+    'RULE' => NULL,
+    'ID' => 'demo:news',
+    'PATH' => '/examples/my-components/news_sef.php',
+    'SORT' => 100,
+  ),
+  1 => 
+  array (
+    'CONDITION' => '#^/bitrix/services/ymarket/#',
+    'RULE' => '',
+    'ID' => '',
+    'PATH' => '/bitrix/services/ymarket/index.php',
+    'SORT' => 100,
+  ),
+  5 => 
+  array (
+    'CONDITION' => '#^/e-store/books/reviews/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/e-store/books/reviews/index.php',
+    'SORT' => 100,
+  ),
+  6 => 
+  array (
+    'CONDITION' => '#^/e-store/xml_catalog/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog',
+    'PATH' => '/e-store/xml_catalog/index.php',
+    'SORT' => 100,
+  ),
+  8 => 
+  array (
+    'CONDITION' => '#^/communication/forum/#',
+    'RULE' => '',
+    'ID' => 'bitrix:forum',
+    'PATH' => '/communication/forum/index.php',
+    'SORT' => 100,
+  ),
+  10 => 
+  array (
+    'CONDITION' => '#^/communication/blog/#',
+    'RULE' => '',
+    'ID' => 'bitrix:blog',
+    'PATH' => '/communication/blog/index.php',
+    'SORT' => 100,
+  ),
+  0 => 
+  array (
+    'CONDITION' => '#^/stssync/calendar/#',
+    'RULE' => '',
+    'ID' => 'bitrix:stssync.server',
+    'PATH' => '/bitrix/services/stssync/calendar/index.php',
+    'SORT' => 100,
+  ),
+  2 => 
+  array (
+    'CONDITION' => '#^/content/articles/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/content/articles/index.php',
+    'SORT' => 100,
+  ),
+  9 => 
+  array (
+    'CONDITION' => '#^/content/gallery/#',
+    'RULE' => '',
+    'ID' => 'bitrix:photogallery_user',
+    'PATH' => '/content/gallery/index.php',
+    'SORT' => 100,
+  ),
+  7 => 
+  array (
+    'CONDITION' => '#^/personal/lists/#',
+    'RULE' => '',
+    'ID' => 'bitrix:lists',
+    'PATH' => '/personal/lists/index.php',
+    'SORT' => 100,
+  ),
+  4 => 
+  array (
+    'CONDITION' => '#^/e-store/books/#',
+    'RULE' => '',
+    'ID' => 'bitrix:catalog',
+    'PATH' => '/e-store/books/index.php',
+    'SORT' => 100,
+  ),
+  3 => 
+  array (
+    'CONDITION' => '#^/content/news/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/content/news/index.php',
+    'SORT' => 100,
+  ),
+  11 => 
+  array (
+    'CONDITION' => '#^/content/idea/#',
+    'RULE' => '',
+    'ID' => 'bitrix:idea',
+    'PATH' => '/content/idea/index.php',
+    'SORT' => 100,
+  ),
+  13 => 
+  array (
+    'CONDITION' => '#^/club/gallery/#',
+    'RULE' => '',
+    'ID' => 'bitrix:photogallery_user',
+    'PATH' => '/club/gallery/index.php',
+    'SORT' => 100,
+  ),
+  16 => 
+  array (
+    'CONDITION' => '#^/content/faq/#',
+    'RULE' => '',
+    'ID' => 'bitrix:support.faq',
+    'PATH' => '/content/faq/index.php',
+    'SORT' => 100,
+  ),
+  14 => 
+  array (
+    'CONDITION' => '#^/club/forum/#',
+    'RULE' => '',
+    'ID' => 'bitrix:forum',
+    'PATH' => '/club/forum/index.php',
+    'SORT' => 100,
+  ),
+  12 => 
+  array (
+    'CONDITION' => '#^/club/#',
+    'RULE' => '',
+    'ID' => 'bitrix:socialnetwork',
+    'PATH' => '/club/index.php',
+    'SORT' => 100,
+  ),
+);
