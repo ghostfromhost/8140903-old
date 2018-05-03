0fb22ed616042f29edd9b594b7cfc50d6e9cdc71
Ivan Ivanov <nuclearfeces@gmail.com>
2018-05-03 19:22:03 +0400

    Main site template have been created

---
+++ local/templates/main/include_areas/contacts.php
@@ -0,0 +1,8 @@
+<div class="padleft">
+<h4>Contacts</h4>
+<p>Curabitur placerat, urna eu fringilla placerat, urna eu venenatis</p>
+
+<b class="big">T: 800-232-2321</b><br/>
+<b class="big">F: 800-231-2313</b><br/>
+<a href="#"><? echo COption::GetOptionString("main", "email_from"); ?></a><br/>
+</div>
---
+++ local/templates/main/include_areas/logo.php
@@ -0,0 +1,3 @@
+<?if(!CSite::InDir('/')):?><a href="/"><?endif;?>
+<img src=<?=SITE_TEMPLATE_PATH;?>"/images/logo.png" alt="Novux" />
+<?if(!CSite::InDir('/')):?></a><?endif;?>
