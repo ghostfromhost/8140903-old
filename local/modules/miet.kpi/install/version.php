258e4d0595549259561576b1350fdd63be290e06
Ivan Ivanov <nuclearfeces@gmail.com>
2018-05-07 21:52:21 +0400

    KPI module skeleton has been created

---
+++ local/modules/miet.kpi/install/version.php
@@ -0,0 +1,6 @@
+<?php
+	$arModuleVersion = array(
+	"VERSION" => "1.0.0",
+	"VERSION_DATE" => "YYYY-MM-DD HH:MM:SS"
+	);
+?>
---
+++ local/modules/miet.kpi/lang/en/install/index.php
@@ -0,0 +1,7 @@
+<?php
+$MESS["miet.kpi_MODULE_NAME"] = "MIET. KPI";
+$MESS["miet.kpi_MODULE_DESC"] = "Evaluation of KPI staff and
+units";
+$MESS["miet.kpi_PARTNER_NAME"] = "MIET";
+$MESS["miet.kpi_PARTNER_URI"] = "http://miet.ru/";
+?>
